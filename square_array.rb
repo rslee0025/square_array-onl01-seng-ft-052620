1554cd47efa07a9b8cea3ae189f4ad45b051ea32
Rachel S. Lee <rslee0025@gmail.com>
2020-05-31 16:59:12 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,31 @@
-def square_array(array)
-  new_array = []
-  array.each do |num|
-    new_array << (num**2)
-end
-return new_array
-end
+#def square_array(array)
+#  new_array = []
+#  array.each do |num|
+#    new_array << (num**2)
+#end
+#return new_array
+#end
+
+def square_array
+  
+end  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
